2769429d1c48885b0d306f2c3390a1a63e737487
Dilip-kumar-Anjana <152201000+Dilip-kumar-Anjana@users.noreply.github.com>
2025-03-04 00:05:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("section");
+
+  function checkScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      if (sectionTop < window.innerHeight - 100) {
+        section.classList.add("visible");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", checkScroll);
+  checkScroll(); // Initial check
+});
+
